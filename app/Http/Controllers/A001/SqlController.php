d4da03205f2fc6e281746e69ca378c4adf462f2b
requenahdz <requenahdz@gmail.com>
2023-10-13 13:13:56 -0600

    show tables

--- app/Http/Controllers/A001/SqlController.php
+++ app/Http/Controllers/A001/SqlController.php
@@ -7,6 +7,7 @@ use App\Models\A001\Sitie;
 use Illuminate\Support\Facades\Response;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class SqlController extends Controller
 {
@@ -18,5 +19,16 @@ class SqlController extends Controller
     public function run()
     {
         return Artisan::call('sql:run');
+    }
+
+    public function tabla($table = null)
+    {
+
+        if ($table) {
+            $response =  DB::table($table)->get();
+        } else {
+            $response =  collect(DB::select('SHOW TABLES'))->pluck('Tables_in_' . env('DB_DATABASE'));
+        }
+        return Response::json($response, 200);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -35,9 +35,8 @@ Route::resource('A001/apps', A001AppsController::class);
 Route::resource('A001/sites', A001SitesController::class);
 Route::get('A001/sql/backup', [A001SqlController::class, 'backup']);
 Route::get('A001/sql/run', [A001SqlController::class, 'run']);
+Route::get('A001/sql/table/{name?}', [A001SqlController::class, 'tabla']);
 
 //A002
 Route::resource('A002/payments', A002PaymentsController::class);
 Route::resource('A002/loans', A002LoansController::class);
-
-
